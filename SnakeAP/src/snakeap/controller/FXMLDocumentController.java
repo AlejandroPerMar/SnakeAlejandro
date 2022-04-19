5df5dcaad06dfce69ed2cbfaf3ab1fa180c42dad
Alejandro Pérez Martín <aleperzmartin@gmail.com>
2022-04-19 11:50:35 +0100

    Se realiza la estructura del FXML añadiendo el pane, canvas y botones como en el ejemplo base.

--- SnakeAP/src/snakeap/controller/FXMLDocumentController.java
+++ SnakeAP/src/snakeap/controller/FXMLDocumentController.java
@@ -7,29 +7,36 @@ package snakeap.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 
 /**
+ * FXML Controller class
  *
  * @author aleja
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Canvas cvEscenario;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button btnInciarPausar;
+    @FXML
+    private Button btnCrearPartida;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void moverSnake(KeyEvent event) {
+    }
+
 }
